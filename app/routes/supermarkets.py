e03cc305b47382d3482207f127ab353e3fa2937f
alex <alexjesus0116@gmail.com>
2023-09-03 23:45:22 -0300

    nova rota
    criada nova rota get_supermarket para busca de um único supermarket por
    seu id

--- app/routes/supermarkets.py
+++ app/routes/supermarkets.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 
 from app.repositories.supermarkets import SupermarketRepository
+from app.schemas.supermarket import Supermarket
 
 supermarket_router = APIRouter(
     prefix="/supermarkets"
@@ -12,8 +13,18 @@ async def get_supermarkets():
 
     if not supermarkets:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Nenhum supermercado registrado"
         )
-
     return supermarkets
+
+@supermarket_router.get("/{id}")
+async def get_supermarket(id: int):
+    supermarket = await SupermarketRepository.get_by_id(id)
+
+    if not supermarket:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Nenhum supermercado registrado"
+        )
+    return supermarket
