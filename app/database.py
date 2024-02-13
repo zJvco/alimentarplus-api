50b2190bf1fdcc2f9da24d3852f42a181ff2526d
zJvco <65981077+zJvco@users.noreply.github.com>
2024-02-13 15:44:12 -0300

    Implementação da lógica que verifica se o ambiente é desenvolvimento ou produção juntamente com a implementação do banco de dados mysql no código

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-
 if os.getenv("ENVIRONMENT") == "development":
     engine = create_async_engine(
         os.getenv("DEV_DB_URI"),
@@ -10,8 +9,15 @@ if os.getenv("ENVIRONMENT") == "development":
         echo=True
     )
 elif os.getenv("ENVIRONMENT") == "production":
+
+    if os.getenv("PROD_DB_TYPE") == "mysql":
+        connection_string = f"mysql+asyncmy://{os.getenv('PROD_DB_USER')}:{os.getenv('PROD_DB_PASSWORD')}@{os.getenv('PROD_DB_HOST')}/{os.getenv('PROD_DB_NAME')}?charset=utf8mb4"
+    else:
+        connection_string = ""
+
     engine = create_async_engine(
-        os.getenv("PROD_DB_URI")
+        connection_string,
+        echo=False
     )
 else:
     engine = create_async_engine(
